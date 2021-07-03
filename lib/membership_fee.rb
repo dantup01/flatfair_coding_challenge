f812e6e086065c6c71404fa5b6e16b0e816c12bd
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-07-03 12:50:48 +0100

    add validation tests and create validations

--- lib/membership_fee.rb
+++ lib/membership_fee.rb
@@ -4,8 +4,31 @@ require 'json'
 file = File.read('lib/config.json')
 ORG_STRUCTURE = JSON.parse(file)
 
-# Method to determine if a branch has a fixed fee
+# Set required constants. All monetary values stored in pence
+VAT = 1.2
 
+MIN_RENT_WEEK = 2500
+MIN_RENT_MONTH = 11000
+
+MAX_RENT_WEEK = 200000
+MAX_RENT_MONTH = 866000
+
+# Create data models
+class OrganisationUnit
+  def initialize(attributes = {})
+    @name = attributes[:name]
+    @config = attributes[:OrganisationUnitConfig]
+  end
+end
+
+class OrganisationUnitConfig
+  def initialize(attributes = {})
+    @has_fixed_membership_fee = attributes[:has_fixed_membership_fee]
+    @fixed_membership_fee_amount = attributes[:fixed_membership_fee_amount]
+  end
+end
+
+# Method to determine if a branch has a fixed fee
 def org_unit_has_fixed_fee(organisation_unit)
   name = ORG_STRUCTURE.find { |h1| h1['name'] == "branch_#{organisation_unit}" }
   if name['config'].nil?
@@ -13,5 +36,18 @@ def org_unit_has_fixed_fee(organisation_unit)
     ORG_STRUCTURE.find { |h1| h1['name'] == "#{parent}" }['config']['fixed_membership_fee_amount']
   else
     name['config']['fixed_membership_fee_amount']
+  end
+end
+
+# Validation method
+def validation_rent_amount(rent_amount, rent_period)
+  if rent_amount < MIN_RENT_WEEK && rent_period == 'week'
+    raise TypeError.new('Weekly rent_amount must be above minimum value of 2500')
+  elsif rent_amount < MIN_RENT_MONTH && rent_period == 'month'
+    raise TypeError.new('Monthly rent_amount must be above minimum value of 11000')
+  elsif rent_amount > MAX_RENT_WEEK && rent_period == 'week'
+    raise TypeError.new('Weekly rent_amount must be below maximum value of 200000')
+  elsif rent_amount > MAX_RENT_MONTH && rent_period == 'month'
+    raise TypeError.new('Monthly rent_amount must be below maximum value of 866000')
   end
 end
--- spec/membership_fee_spec.rb
+++ spec/membership_fee_spec.rb
@@ -14,3 +14,20 @@ context 'branch_m has no config so returns area_d and' do
   it { expect(org_unit_has_fixed_fee('m')).to eq(0) }
 end
 
+# Test validations of min/max weekly/monthly rent amounts
+
+context 'Validation of min weekly rent amount' do
+  it { expect { validation_rent_amount(2400, 'week') }.to raise_error('Weekly rent_amount must be above minimum value of 2500') }
+end
+
+context 'Validation of min monthly rent amount' do
+  it { expect { validation_rent_amount(10000, 'month') }.to raise_error('Monthly rent_amount must be above minimum value of 11000') }
+end
+
+context 'Validation of max weekly rent amount' do
+  it { expect { validation_rent_amount(200001, 'week') }.to raise_error('Weekly rent_amount must be below maximum value of 200000') }
+end
+
+context 'Validation of max monthly rent amount' do
+  it { expect { validation_rent_amount(866001, 'month') }.to raise_error('Monthly rent_amount must be below maximum value of 866000') }
+end
